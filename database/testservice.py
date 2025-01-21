0e7b988872cbd5cce17f1cf823c779b73250162d
Same272 <umarbekusmanovich@gmail.com>
2025-01-21 18:45:03 +0500

    h

--- database/testservice.py
+++ database/testservice.py
@@ -1,6 +1,16 @@
 from database import get_db
 from database.models import *
 
+def add_question_db(main_question, v1, v2, correct_answer, level,
+                    v3=None, v4=None):
+    with next(get_db()) as db:
+        new_question = Question(main_question=main_question,
+                                v1=v1, v2=v2, v3=v3, v4=v4,
+                                correct_answer=correct_answer,
+                                level=level)
+        db.add(new_question)
+        db.commit()
+        return True
 
 def get_top5_db(level, ):
     with next(get_db()) as db:
